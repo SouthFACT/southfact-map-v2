4b92cf9729171f749eadf800d2e537ac9f315269
Dave Michelson <daveism@gmail.com>
2022-04-18 11:11:37 -0400

    removed duplicate

--- src/config/navConfig.js
+++ src/config/navConfig.js
@@ -5,49 +5,48 @@ export var navConfig = {
       ref: "main-nav-home",
       text: "Home",
       id: "main-nav-home",
-      href: "https://southfact.github.io/southfact-gatsby-site/"
-   },
-   {
-     name: "map",
-     ref: "main-nav-map",
-     text: "Map",
-     id: "main-nav-map",
-     href: "./#Home"
-  },
-  {
-    name: "cr",
-    ref: "main-nav-cr",
-    text: "Custom Requests",
-    id: "main-nav-cr",
-    href: "https://code.earthengine.google.com/99d055f769538474c2e566a764f6eb6d?hideCode=true"
-   },
-   {
-     name: "cr",
-     ref: "main-nav-cr",
-     text: "Custom Requests",
-     id: "main-nav-cr",
-     href: "https://code.earthengine.google.com/99d055f769538474c2e566a764f6eb6d?hideCode=true"
-  },
-  {
-    name: "learn",
-    ref: "main-nav-learn",
-    text: "Learn",
-    id: "main-nav-learn",
-    href: "https://southfact.github.io/southfact-gatsby-site/about"
-  },
-  {
-    name: "downloads",
-    ref: "main-nav-downloads",
-    text: "Downloads",
-    id: "main-nav-downloads",
-    href: "https://southfact.github.io/southfact-gatsby-site/downloads"
-  },
-  {
-    name: "contact",
-    ref: "main-nav-contact",
-    text: "Contact",
-    id: "main-nav-contact",
-    href: "https://southfact.github.io/southfact-gatsby-site/contact"
-  }
- ]
+      href: "https://southfact.github.io/southfact-gatsby-site/",
+      target: "_self"
+    },
+    {
+      name: "map",
+      ref: "main-nav-map",
+      text: "Map",
+      id: "main-nav-map",
+      href: "./#Home",
+      target: "_self"
+    },
+    {
+      name: "cr",
+      ref: "main-nav-cr",
+      text: "Custom Requests",
+      id: "main-nav-cr",
+      href: "https://code.earthengine.google.com/99d055f769538474c2e566a764f6eb6d?hideCode=true",
+      target: "_blank"
+    },
+    {
+      name: "learn",
+      ref: "main-nav-learn",
+      text: "Learn",
+      id: "main-nav-learn",
+      href: "https://southfact.github.io/southfact-gatsby-site/about",
+      target: "_self"
+    },
+    {
+      name: "downloads",
+      ref: "main-nav-downloads",
+      text: "Downloads",
+      id: "main-nav-downloads",
+      href: "https://southfact.github.io/southfact-gatsby-site/downloads",
+      target: "_self"
+    },
+    {
+      name: "contact",
+      ref: "main-nav-contact",
+      text: "Contact",
+      id: "main-nav-contact",
+      href: "https://southfact.github.io/southfact-gatsby-site/contact",
+      target: "_self"
+    }
+  ]
 }
